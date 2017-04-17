f8f95df40e9eba33d1ae10607f19b3035da17ae5
DapperMapper14 <jonathan.a.sullivan@gmail.com>
2017-04-17 15:32:34 -0400

    added a 'most_imporant' function that selects nodes with highest centrality measure

--- graph_stats.py
+++ graph_stats.py
@@ -8,7 +8,26 @@ def topfive(centrality_rank):
     for key,value in rank_sorted[0:5]:
         print "highest: ",key,value
 
-def plot_degree_distribution (g, graphMeasure, plotTitle, oName) :
+def most_important(G, centralityMeasure="betweenness", cutoff=3):
+	if centralityMeasure == "betweenness":
+		ranking = nx.betweenness_centrality(G).items()
+	elif centralityMeasure == "closeness":
+		ranking = nx.closeness_centrality(G).items()
+	elif centralityMeasure == "degree":
+		ranking = nx.degree_centrality(G).items()
+	else:
+		raise "Invalid centrality measure. str type of 'betweenness', 'closeness', or 'degree'"
+
+	r = [x[1] for x in ranking]
+	m = sum(r)/len(r) # mean centrality
+	t = m*cutoff # threshold, we keep only the nodes with 3 times the mean
+	Gt = G.copy()
+	for k, v in ranking:
+		if v < t:
+			Gt.remove_node(k)
+	return Gt
+
+def plot_degree_distribution (g, plotTitle, oName):
 	degs = {}
 	for n in g.nodes():
 		deg = g.degree(n)
