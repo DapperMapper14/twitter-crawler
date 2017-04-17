a50e49862dac7edaff3a340e00afd262e6906dc3
DapperMapper14 <jonathan.a.sullivan@gmail.com>
2017-04-17 00:02:22 -0400

    converted script to function

--- build_nw.py
+++ build_nw.py
@@ -1,42 +1,41 @@
 import json
 import networkx as nx
-import matplotlib.pyplot as plt
-data=[]
-G=nx.Graph()
-names=[]
-with open("twitter-data/march-madness-final.json") as tfile:
-	for line in tfile:
+
+def twitter_graph(twitterJSON, G=nx.Graph()):
+	data=[]
+	names=[]
+	with open(twitterJSON) as tfile:
+		for line in tfile:
+			try:
+				data.append(json.loads(line))
+			except:
+				continue
+	# generate user list and add nodes
+	for d in data:
 		try:
-			data.append(json.loads(line))
-		except:
+			name=d["user"]["name"]
+			if name not in names:
+				names.append(name)
+				G.add_node(name, size=1)
+			else:
+				size=G.node[name]["size"]
+				size+=5
+				G.add_node(name,size=size)
+		except KeyError:
 			continue
-# generate user list and add nodes
-for d in data:
-	try:
-		name=d["user"]["name"]
-		if name not in names:
-			names.append(name)
-			G.add_node(name, size=1)
-		else:
-			size=G.node[name]["size"]
-			size+=5
-			G.add_node(name,size=size)
-	except KeyError:
-		continue
-# add edges between users and mentions
-for d in data:
-	try:
-		name=d["user"]["name"]
-		mentions=d["entities"]["user_mentions"]
-		for m in mentions:
-			if m["name"] in names:
-				if not G.has_edge(name, m["name"]):
-					G.add_edge(name,m["name"],weight=5)
-				else:
-					weight=G[name][m["name"]]["weight"]
-					weight+=5
-					G.add_edge(name,m["name"],weight=weight)
-	except KeyError:
-		continue
-nx.draw(G)
-plt.show()
+	# add edges between users and mentions
+	for d in data:
+		try:
+			name=d["user"]["name"]
+			mentions=d["entities"]["user_mentions"]
+			for m in mentions:
+				if m["name"] in names:
+					if not G.has_edge(name, m["name"]):
+						G.add_edge(name,m["name"],weight=5)
+					else:
+						weight=G[name][m["name"]]["weight"]
+						weight+=5
+						G.add_edge(name,m["name"],weight=weight)
+		except KeyError:
+			continue
+	return G
